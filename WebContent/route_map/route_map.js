d4edb1887c5e24884062c5de6b407fce3edcdd54
RareScrap <RareScrap@users.noreply.github.com>
2017-09-08 08:55:23 +0800

    ТЕСТ: отображаются точки в изгибах маршрута

--- WebContent/route_map/route_map.js
+++ WebContent/route_map/route_map.js
@@ -3,11 +3,17 @@ function init () {
 	 * Тестовый массив с метками адресов
 	 */
 	var arr = new Array();
-	for(var i=0; i<window.getDataJSInterface.getRoutePointsNumber(); i++){
+	arr[0] = new Array();
+    arr[0][0] = 52.289187;
+    arr[0][1] = 104.280826;
+    arr[1] = new Array();
+    arr[1][0] = 52.274419;
+    arr[1][1] = 104.253159;
+	/*for(var i=0; i<window.getDataJSInterface.getRoutePointsNumber(); i++){
 	    arr[i] = new Array();
 	    arr[i][0] = window.getDataJSInterface.getRoutePointCoord(i, 0);
 	    arr[i][1] = window.getDataJSInterface.getRoutePointCoord(i, 1);
-	  }
+	  }*/
 	
     /**
      * Создаем мультимаршрут.
@@ -24,6 +30,8 @@ function init () {
         boundsAutoApply: true
     });
 
+    ymaps
+    
     // Создаем карту с добавленными на нее кнопками.
     var myMap = new ymaps.Map('map', {
         center: [55.750625, 37.626],
@@ -35,19 +43,54 @@ function init () {
    
     // Ожидаем успешного построения маршрута
     multiRoute.model.events.add("requestsuccess", function(){
-		window.updateDataJSInterface.updateRouteLength( multiRoute.getActiveRoute().properties.get('distance').value );
+		//window.updateDataJSInterface.updateRouteLength( multiRoute.getActiveRoute().properties.get('distance').value );
 		
 		// Возвращает пиздец, если расстояние огромно
 		//window.console.log( multiRoute.getActiveRoute().properties.get('durationInTraffic').value );
 		
 		var s = multiRoute.getActiveRoute().properties.get('duration').value;
+		var test = multiRoute.getActiveRoute().properties.get('coordinates');
 		var m = (s/60) % 60;
 		window.console.log(m);
 		var h = s / 3600;
 		window.console.log(h);
 		
-		window.updateDataJSInterface.updateTripTime(h, m);
+		//window.updateDataJSInterface.updateTripTime(h, m);
+		
+		var moveList = 'Трогаемся,</br>',
+	        way,
+	        segments;
+	    // Получаем массив путей.
+		for (var g = 0; g < multiRoute.getRoutes().getLength(); g++) {
+			var route = multiRoute.getRoutes().get(g);
+		    for (var i = 0; i < route.getPaths().getLength(); i++) {
+		        way = route.getPaths().get(i);
+		        segments = way.getSegments();
+		        
+		        segments = segments._collectionComponent._baseArrayComponent._children;
+		        
+		        for (var j = 0; j < segments.length; j++) {
+		        	var myPlacemark = createPlacemark(segments[j].model.geometry._coordPath._coordinates[0]);
+					myMap.geoObjects.add(myPlacemark);
+		        	
+		            /*var street = segments[j].getStreet();
+		            moveList += ('Едем ' + segments[j].getHumanAction() + (street ? ' на ' + street : '') + ', проезжаем ' + segments[j].getLength() + ' м.,');
+		            moveList += '</br>'*/
+		        }
+		    }
+		}
+	    moveList += 'Останавливаемся.';
+		//var test = multiRoute.getActiveRoute().getPaths().getLength()/*.get('coordinates')*/;
     });
+    
+    function createPlacemark(coords) {
+		return new ymaps.Placemark(coords, {
+			iconCaption: 'поиск...'
+		}, {
+			preset: 'islands#violetDotIconWithCaption',
+			draggable: false
+		});
+	}
 
     // Добавляем мультимаршрут на карту.
     myMap.geoObjects.add(multiRoute);
